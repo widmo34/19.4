1349a8c40b8bbd48bab2515c806cf7d46811683c
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-08-20 18:45:02 +0200

    19.4 alpha haha

--- actions.js
+++ actions.js
@@ -1,53 +1,50 @@
-import uuid from 'uuid';
+import uuid from uuid;
 
-export const ADD_COMMENT = 'ADD_COMMENT';
-export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const ADD_COMMENT  = 'ADD_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
-
-
-function addComment(text) {
-    return {
-        type: ADD_COMMENT,
-        text,
-        id: uuid.v4()
-    }
-}
-
-
-function editComment(id) {
-    return {
-        type: EDIT_COMMENT,
-        text,
-        id: id,
-     }
-}
-
-
-function removeComment(id) {
-    return {
+function removeComment(id){
+    return{
         type: REMOVE_COMMENT,
         id: id
     }
 }
 
+function addComment(text){
+    return{
+        type: ADD_COMMENT,   
+        id: uuid.v4(),
+        text,
+        votes: 0
 
-function thumbUpComment(id, up)  {
-    return {
-        type: THUMB_UP_DOWN_COMMENT,
-        id: id,
-        countUp: up
     }
 }
 
-function thumbDownComment(id, down)  {
-    return {
-        type: THUMB_UP_DOWN_COMMENT,
+function editComment(id, text){
+    return{
+        type: EDIT_COMMENT,
         id: id,
-        countDown: down 
+        text: text,
+        votes
+
     }
 }
 
+function thumbUpComment(id){
+    return{
+        type: THUMB_UP_COMMENT,
+        id: id,
+      
+    }
+}
 
+function thumbDownComment(id){
+    return{
+        type: THUMB_DOWN_COMMENT,
+        id: id,
+      
+    }
+}
--- reducer.js
+++ reducer.js
@@ -1,66 +1,10 @@
+// reducer.js
 
-const initializeState = {
-    comments: [],
-    users: []
-}
+import { combineReducers} from  'redux';
+import comments from './comments';
+import users from  './users';
 
-import {ADD_COMMENT} from './actions'
-import {REMOVE_COMMENT} from './actions'
-import  {EDIT_COMMENT} from './actions'
-import  {THUMB_UP_COMMENT} from './actions'
-import  {THUMB_DOWN_COMMENT}from './actions'
-import {comments} from './comments'
-import { combineReducers } from 'redux';
-
-
-function reducer(state = initializeState, action){
-    switch(action.type){
-       
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
-        
-        case EDIT_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.forEach(element => {
-                    if(element.id === action.id){
-                        {
-                         element.text = action.text;
-                        }
-                    }
-                })
-            } 
-        )
-        
-        case THUMB_UP_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.forEach(element => {
-                    if(element.id === action.id){
-                        {
-                         element.countUp += 1;
-                        }
-                    }
-                })
-            })
-         
-            case THUMB_DOWN_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.forEach(element => {
-                    if(element.id === action.id){
-                        {
-                         element.countDown += 1;
-                        }
-                    }
-                })
-            })    
-       
-       
-        default:
-                return state;
-    }
-};
-
-
-
-
+const app = combineReducers({
+    comments,
+    users
+})
